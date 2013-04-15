a8d91c2b9b3df5175c279e3f9c6da66e626ce2fc
Travis Martin <nitramsivart@gmail.com>
2013-04-14 22:02:14 -0400

    just adding some dummy files

---
+++ directscal.c
@@ -0,0 +1,41 @@
+/* 
+ * Currently just a copy of cnt
+*/
+
+#include <gmp.h>
+
+#define w		(GMP_NUMB_BITS/2)
+#define _BOTMASK	((1ul << w)-1)
+#define _TOPMASK	(~_BOTMASK)
+#define BOT(x)		((x) & _BOTMASK)
+#define TOP(x)		((x) >> w)
+#define LIMB(z,i)	(((i)<((z)->_mp_size))?((z)->_mp_d[i]):(0L))
+#define BOTL(z,i)	(BOT(LIMB(z,i)))
+#define TOPL(z,i)	(TOP(LIMB(z,i)))
+#define HLIMB(z,j)	((j&1)?(TOPL(z,j>>1)):(BOTL(z,j>>1)))
+
+unsigned getGMP_NUMB_BITS()
+{
+  return GMP_NUMB_BITS;
+}
+
+unsigned long directScal(unsigned long kap, mpz_t cz, mpz_t yz)
+{
+  unsigned long nW=(kap+1)/(2*w), val=0, i;
+
+  if(nW*w*2 != kap+1)
+      return 0;
+
+  for(i = 0; i < nW-1; i++) {
+    val += BOTL(cz,i) * LIMB(yz,nW-1-i);
+    val += (BOTL(cz,i) * TOPL(yz,nW-2-i)) >> w;
+    val += TOPL(cz,i) * ((BOTL(yz,nW-1-i) << w) + TOPL(yz,nW-2-i));
+    val += (TOPL(cz,i) * BOTL(yz,nW-2-i)) >> w;
+  }
+
+  val += BOTL(cz,nW-1) * LIMB(yz,0);
+  val += TOPL(cz,nW-1) * (BOTL(yz,0) << w);
+
+  return val;
+}
+
