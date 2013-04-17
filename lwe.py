688ddd2c8a4b13128224900996bccd42a65d2fba
Travis Martin <nitramsivart@gmail.com>
2013-04-16 21:57:06 -0400

    partial working relinearization using sage polynomials

--- lwe.py
+++ lwe.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_error(q):
-  return random.randint(0, q/10)
+  return random.randint(0, q**int(1.0/4))
 
 def dot(v1, v2):
   sum = 0
@@ -13,26 +13,63 @@ class Sk:
   def __init__(self, q, n):
     self.q = q
     self.n = n
-    self.z = PolynomialRing(Integers(q),n,"z").gens()
-    self.s = [random.randint(0, q-1) for i in range(n)]
+    self.svars = PolynomialRing(Integers(q),n,"s").gens()
+    self.s = self.randlist(q)
+    self.tvars = PolynomialRing(Integers(q),n,"t").gens()
+    self.t = self.randlist(q)
+    self.sencrypts = [self.encrypt2(self.s[i]) for i in range(n)]
+    self.s2encrypts = [[self.encrypt2(self.s[i]*self.s[j]) for i in range(n)] for j in range(n)]
 
-  def encrypt(self, m):
-    a = [random.randint(0, self.q-1) for i in range(self.n)]
-    e = generate_error(self.q**(1/4))
+  def randlist(self, q):
+    return [random.randint(0, int(q**.125)) for i in range(self.n)]
+
+  # first level of encrypt
+  def encrypt1(self, m):
+    a = self.randlist(self.q)
+    print a
+    e = generate_error(self.q)
     b = dot(a, self.s) + 2*e + m
     f = b
     for i in range(self.n):
-      f = f - a[i] * self.z[i]
+      f = f - a[i] * self.svars[i]
     return f
+
+  # second level of encrypt, encrypting s values
+  def encrypt2(self, s):
+    a = self.randlist(self.q)
+    e = generate_error(self.q)
+    b = dot(a, self.t) + 2*e + s
+    f = b
+    for i in range(self.n):
+      f = f - a[i] * self.tvars[i]
+
+  # turns a quadratic function into a linear function
+  def relinearize(self, f):
+    n = self.n
+    #compute constant term
+    newf = f([0]*n)
+    #compute linear terms
+    lin = [f.coefficient(self.svars[i])([0]*n) for i in range(n)]
+    for i in range(n):
+      newf = newf + self.sencrypts[i] * f.coefficient(self.svars[i])([0]*n)
+    for i in range(n):
+      for j in range(n):
+        if i == j:
+          coeff = f.coefficient(self.svars[i]**2)
+        else:
+          coeff = (f.coefficient(self.svars[i])).coefficient(self.svars[j])
+        newf = newf + self.sencrypts[i][j]
+    return quad
 
   def decrypt(self, f):
     return f(self.s).lift().mod(2)
 
-sk = Sk(1000001, 3)
-f = sk.encrypt(1)
-print f
-f2 = sk.encrypt(0)
-print f2
-print f * f2
-print sk.decrypt(f*f2)
-#print sk.z
+sk = Sk(100000001.0, 2)
+f1 = sk.encrypt1(1)
+print f1
+f2 = sk.encrypt1(0)
+f3 = f1*f2
+print f3
+f4 = sk.relinearize(f3)
+print f4
+#print sk.decrypt(f4)
