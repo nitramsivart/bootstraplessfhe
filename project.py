443a0507cc9cef858a7789c959dd2c5533762b9a
Meghan Clark <yourspamhere123@gmail.com>
2013-04-18 04:07:28 -0400

    got rid of private class

--- project.py
+++ project.py
@@ -1,6 +1,33 @@
 import random
 import math
 
+def main():
+  # public info 
+  q = 2**10
+  n = 10
+  logq = int(math.ceil(math.log(q, 2)))
+
+  # private info
+  s = randlist(q, n)
+  svars = PolynomialRing(Integers(q),n,"s").gens()
+
+  # main logic
+  print "encrypting bit - please wait"
+  f1 = encrypt(1, s, svars, q, n)
+  print "encrypting bit - please wait"
+  f2 = encrypt(1, s, svars, q, n)
+  print "adding bit - please wait"
+  f3 = f1+f2
+  print "multiplying bit - please wait"
+  f4 = f1*f2
+  print "relinearizing"
+  f5 = relinearize(f4, svars, n)
+  #print f4
+  print "decrypting bit - please wait"
+  print decrypt(f1, s)
+  print "decrypting bit - please wait"
+  print decrypt(f2, s)
+
 def generate_error(q):
   return random.randint(0, q>>20)
 
@@ -10,49 +37,25 @@ def dot(v1, v2):
     sum += v1[i] * v2[i]
   return sum
 
-class Sk:
-  def __init__(self, q, n):
-    # auxilary (public) information
-    self.q = q
-    self.n = n
-    self.logq = int(math.ceil(log(q, 2)))
-    # private information
-    self.s = self.randlist(q)
-    self.svars = PolynomialRing(Integers(q),n,"s").gens()
+def randlist(q, n):
+  return [random.randint(0, q) for i in range(n)]
 
-  def randlist(self, q):
-    return [random.randint(0, q) for i in range(self.n)]
+# encrypt the bit m
+def encrypt(m, s, svars, q, n):
+  a = randlist(q, n)
+  e = generate_error(q)
+  b = dot(a, s) + 2*e + m
+  return b - dot( a, svars)
 
-  # encrypt the bit m
-  def encrypt(self, m):
-    a = self.randlist(self.q)
-    e = generate_error(self.q)
-    b = dot(a, self.s) + 2*e + m
-    return b - dot( a, self.svars )
-  # decrypt the ciphertext c
-  def decrypt(self, c, key):
-    return c(key).lift().mod(2)
+# decrypt the ciphertext c
+def decrypt(c, key):
+  return c(key).lift().mod(2)
 
-  # server side functions
-  def relinearize(self, f):
-    n = self.n
-    g = f([0 for i in range(n)])
-    lin_coeff = [f.coefficient(self.svars[i]) for i in range(n)]
-    print lin_coeff
+# server side functions
+def relinearize(f, svars, n):
+  n = n
+  g = f([0 for i in range(n)])
+  lin_coeff = [f.coefficient(svars[i]) for i in range(n)]
+  print lin_coeff
 
-sk = Sk(2**20, 1024)
-print "encrypting bit - please wait"
-f1 = sk.encrypt(1)
-print "encrypting bit - please wait"
-f2 = sk.encrypt(1)
-print "adding bit - please wait"
-f3 = f1+f2
-print "multiplying bit - please wait"
-f4 = f1*f2
-print "relinearizing"
-f5 = sk.relinearize(f4)
-#print f4
-print "decrypting bit - please wait"
-print sk.decrypt(f1, sk.s)
-print "decrypting bit - please wait"
-print sk.decrypt(f2, sk.s)
+main()
