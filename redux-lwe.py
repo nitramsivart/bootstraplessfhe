75a5c813737e0e259e16650b9bd290c927bc44e0
Meghan Clark <yourspamhere123@gmail.com>
2013-04-18 03:38:05 -0400

    added template code for fresh start

---
+++ redux-lwe.py
@@ -0,0 +1,22 @@
+#Authors: Meghan Clark, Alex James
+
+"""
+LWE-based FHE implementation
+
+"""
+
+### PARAMETERS ###
+
+def main():
+
+def keygen():
+
+def encrypt():
+
+def decrypt():
+
+def bit_add():
+
+def bit_mult():
+
+main()
